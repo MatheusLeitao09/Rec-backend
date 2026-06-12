639919e4366e790cbff9065ed2e4046cd157673b
Matheus Gomes Leitão <matheus.g.leitao@aluno.senai.br>
2026-06-12 16:14:27 -0300

    first commit

---
+++ src/models/AlunoModel.js
@@ -0,0 +1,60 @@
+import prisma from '../lib/services/prismaClient.js';
+
+export default class AlunoModel {
+    constructor({ id = null, nome, turma, foto, materia } = {}) {
+        this.id = id;
+        this.nome = nome;
+        this.turma = turma;
+        this.foto = foto;
+        this.materia = materia;
+    }
+
+    async criar() {
+        return prisma.aluno.create({
+            data: {
+                nome: this.nome,
+                turma: this.turma,
+                foto: this.foto,
+                materia: this.materia
+            },
+        });
+    }
+
+    async atualizar() {
+        return prisma.aluno.update({
+            where: { id: this.id },
+            data: { nome: this.nome, turma: this.turma, foto: this.foto, materia: this.materia },
+        });
+    }
+
+    async deletar() {
+        return prisma.aluno.delete({ where: { id: this.id } });
+    }
+
+    static async buscarTodos(filtros = {}) {
+        const where = {};
+
+        if (filtros.nome) {
+            where.nome = { contains: filtros.nome, mode: 'insensitive' };
+        }
+        if (filtros.turma) {
+            where.turma = { contains: filtros.turma, mode: 'insensitive' };
+        }
+        if (filtros.materia) {
+            where.materia = { contains: filtros.materia, mode: 'insensitive' };
+        }
+        if (filtros.foto !== undefined) {
+            where.foto = filtros.foto === 'true';
+        }
+
+        return prisma.aluno.findMany({ where });
+    }
+
+    static async buscarPorId(id) {
+        const data = await prisma.aluno.findUnique({ where: { id } });
+        if (!data) {
+            return null;
+        }
+        return new AlunoModel(data);
+    }
+}
---
+++ src/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import 'dotenv/config';
+import alunoRoute from './routes/alunoRoute.js';
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('🚀 API funcionando');
+});
+
+// Rotas
+app.use('/api/alunos', alunoRoute);
+
+app.use((req, res) => {
+    res.status(404).json({ error: 'Rota não encontrada' });
+});
+
+app.listen(PORT, () => {
+    console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
+});
